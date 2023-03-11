dfa26295f5f1974abde3ef585a644023736ad555
Franco Cragnolini <cragnolinifranco123@gmail.com>
2023-03-11 15:47:21 -0300

    listview.separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/screens/listview1_screen.dart';
+import 'package:flutter_components/screens/listview2_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Material App",
-      home: const ListView1Screen(),
+      home: const ListView2Screen(),
     );
   }
 }
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ListView2Screen extends StatelessWidget {
+  const ListView2Screen({super.key});
+
+  final options = const [
+    "fantasy",
+    "call of duty",
+    "god of war",
+    "resident evil"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("List View Tipo 1")),
+      body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+                title: Text(options[index]),
+                trailing: const Icon(Icons.arrow_right_alt_outlined),
+                onTap: () {
+                  final game = options[index];
+                  print(game);
+                },
+              ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: options.length),
+    );
+  }
+}
